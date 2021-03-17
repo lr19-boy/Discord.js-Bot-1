96a309ae5d86be5a54aef4948f6e3799afca9159
lr19-boy <72651696+lr19-boy@users.noreply.github.com>
2021-03-17 17:24:42 +0530

    update index.js

--- app/commands/invite.js
+++ app/commands/invite.js
@@ -4,7 +4,7 @@ module.exports.run = async (client, message, args) => {
   const messageEmbed = new MessageEmbed()
     .setTitle("Invites")
     .addFields(
-      {name: "Bot invite link", value: "Here you go man: https://discord.com/oauth2/authorize?client_id=814460658707202068&permissions=8&scope=bot"}
+      {name: "Bot/server invite link", value: "Your bot/server link"}
     )
     .setColor("BLUE")
    message.channel.send(messageEmbed)
