972bd47968bfe6f40d87ebc8fef34cbacc4920b5
lr19-boy <72651696+lr19-boy@users.noreply.github.com>
2021-07-16 12:38:05 +0530

    rps.js

---
+++ app/commands/rps.js
@@ -0,0 +1,45 @@
+const discord = require('discord.js')
+module.exports = {
+	name: "rps",
+	description: "Play a game of rock, paper and scissors",
+	run: async(client, message, args) => {
+		let embed = new discord.MessageEmbed()
+		.setTitle("Rock Paper Scissors")
+		.setDescription("Add a reaction to one of these emojis to play the game!")
+    .setColor('#0f52ba')
+		.setTimestamp()
+		let msg = await message.channel.send(embed)
+		await msg.react("🗻")
+		await msg.react("✂")
+		await msg.react("📰")
+
+		const filter = (reaction, user) => {
+            return ['🗻', '✂', '📰'].includes(reaction.emoji.name) && user.id === message.author.id;
+        }
+
+        const choices = ['🗻', '✂', '📰']
+        const me = choices[Math.floor(Math.random() * choices.length)]
+        msg.awaitReactions(filter, {max:1, time: 60000, error: ["time"]}).then(
+        	async(collected) => {
+        		const reaction = collected.first()
+        		let result = new discord.MessageEmbed()
+        		.setTitle("RESULT")
+            .setColor('#0f52ba')
+        		.addField("Your choice: ", `${reaction.emoji.name}`)
+        		.addField("My choice: ", `${me}`)
+			await msg.edit(result)
+        		if ((me === "🗻" && reaction.emoji.name === "✂") ||
+                (me === "📰" && reaction.emoji.name === "🗻") ||
+                (me === "✂" && reaction.emoji.name === "📰")) {
+                    message.reply("You lost! Nobody can defeat me!");
+            } else if (me === reaction.emoji.name) {
+                return message.reply("It's a tie!");
+            } else {
+                return message.reply("You won!");
+            }
+        })
+        .catch(collected => {
+                message.reply('Process has been cancelled since you did not respond in time!');
+            })
+}
+}
